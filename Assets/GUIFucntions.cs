759faf46ab49e63e2515e7c60e44c3d1c0665e27
Carlo <carlorez987654321@gmail.com>
2025-12-08 14:05:29 +0800

    MainMenu GUI Update
    Updated main menu GUI With animation

--- Assets/GUIFucntions.cs
+++ Assets/GUIFucntions.cs
@@ -1,10 +1,36 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GUIFucntions : MonoBehaviour
 {
+    [SerializeField] private GameObject SettingsMenu;
+
     public void loadMainMenu()
     {
         SceneManager.LoadScene("GameMenu");
+    }
+
+    public void OpenSettings()
+    {
+        if (!SettingsMenu.activeSelf)
+        {
+            SettingsMenu.SetActive(true);
+            SettingsMenu.GetComponent<Animator>().Play("Settings");
+        }
+        else
+        {
+            StartCoroutine(CloseAfterAnim());
+        }
+    }
+
+    IEnumerator CloseAfterAnim()
+    {
+        Animator anim = SettingsMenu.GetComponent<Animator>();
+        anim.Play("SettingsClose");
+
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+
+        SettingsMenu.SetActive(false);
     }
 }
